5fac338ab27eeac4a53645b7e5446265791c6f66
Simon Guest <me@simonguest.com>
2024-02-16 16:07:55 -0800

    Added model download script

---
+++ models/download.sh
@@ -0,0 +1,2 @@
+curl -OL https://huggingface.co/mys/ggml_llava-v1.5-7b/resolve/main/mmproj-model-f16.gguf?download=true
+curl -OL https://huggingface.co/mys/ggml_llava-v1.5-7b/resolve/main/ggml-model-q4_k.gguf?download=true
